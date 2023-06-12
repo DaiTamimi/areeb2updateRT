d59a66936cd8f7caaf1cac03a2e55a3fc5084c61
nouragg <nouragalharbi@gmail.com>
2023-06-12 12:09:21 -0700

    Initial Commit

---
+++ areeb/areebApp.swift
@@ -0,0 +1,17 @@
+//
+//  areebApp.swift
+//  areeb
+//
+//  Created by Noura Alharbi on 23/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct areebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
